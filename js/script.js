542811923de6d6e192899d25b2bc814009a7e93a
KimHyeoonju <tttymttst@gmail.com>
2024-04-11 11:02:00 +0900

    docs : while 문

--- js/script.js
+++ js/script.js
@@ -1,21 +1,6 @@
-// 이중 for 문
-// 구구단
-// 2단
+let x = -10;
 
-let x = 2;
-let result;
-
-for (let y = 1; y <= 9; y++) {
-  result = x * y;
-  // console.log(result);
-}
-
-let resultAll;
-
-for (let x = 2; x <= 9; x++) {
-  for (let y = 1; y <= 9; y++) {
-    resultAll = x * y;
-
-    console.log(resultAll);
-  }
-}
+do {
+  console.log("안녕");
+  x++;
+} while (x > 0);
